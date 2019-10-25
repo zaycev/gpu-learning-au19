0008c247d107dd8951b09bd76bf0eb8625ee6624
Vladimir Zaytsev <zaytsev@uber.com>
2019-10-24 21:24:58 -0700

    Fix typos

--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -36,14 +36,14 @@ fn main() {
         .build(&events_loop)
         .unwrap();
 
-    // Create API instance and surface where to draw.
+    // Create API instance and surface where we will draw pixels.
     let instance = back::Instance::create(title, 1);
     let mut surface = instance.create_surface(&window);
 
-    // Select adapter (logical GPU).
+    // Select adapter (a logical GPU).
     let adapter = instance.enumerate_adapters().remove(0);
 
-    // Define default pixel format: standart RGB + Alpha channel, 8 bits each.
+    // Define a default pixel format. Here we use standard RGB + Alpha channels, 8 bits each.
     let default_pixel_format = Some(format::Format::Rgba8Srgb);
     let default_channel_format = format::ChannelType::Srgb;
 
@@ -52,7 +52,7 @@ fn main() {
     println!("capabilities {:?}", capabilities);
     println!("modes: {:?}", modes);
 
-    // Check that surface formats contain SRGB or default to SRGBA.
+    // Check that surface formats contain sRGB or default to sRGBA.
     let pixel_format_option = formats.map_or(default_pixel_format, |formats| {
         formats
             .iter()
@@ -64,8 +64,8 @@ fn main() {
     });
     let pixel_format = pixel_format_option.expect("supported pixel format not found");
 
-    // Open a physical device and queues (just 1) for Graphics commands.
-    // Selector function will check if the queues can support output to our surface.
+    // Open a physical device and its queues (we need just 1) for graphics commands.
+    // The selector function will check if the queues can support output to our surface.
     let num_queues = 1;
     let (gpu, queues) = adapter
         .open_with::<_, hal::Graphics>(num_queues, |family| -> bool {
@@ -76,14 +76,12 @@ fn main() {
     // Create our engine for drawing state.
     let mut engine = GraphicsEngine::new(gpu, queues, &size, pixel_format, &mut surface);
 
-    // Window event flags.
-    let mut end_requested = false;
-    let mut cursor_pose: LogicalPosition = LogicalPosition::new(0.0, 0.0);
-
     // Main loop:
     //  1. get interaction event for window.
     //  2. update stat.
     //  3. draw state.
+    let mut end_requested = false;
+    let mut cursor_pose: LogicalPosition = LogicalPosition::new(0.0, 0.0);
     loop {
         // If end requested, exit the loop.
         if end_requested {
@@ -107,13 +105,15 @@ fn main() {
         });
 
         // Update state.
+        // In this example, we only update color which will be used by the engine to clear
+        // the surface.
         let r = cursor_pose.x / size.width;
         let g = cursor_pose.y / size.height;
         let b = 1.0;
         let a = 1.0;
         engine.update([r as f32, g as f32, b, a]);
 
-        // Draw state.
+        // Draw the state.
         match engine.draw() {
             Err(msg) => {
                 panic!("failed to draw: {}", msg);
@@ -123,8 +123,7 @@ fn main() {
     }
 }
 
-/// Engine will incapsulate state managenemt and drawing logic.
-/// Fields that has to be destroyed, should be in some container, e.g. Option or Vec.
+/// Engine will be encapsulating the state management and the drawing logic.
 struct GraphicsEngine<B: Backend> {
     // Physical GPU and its graphics queues.
     pub gpu: B::Device,
@@ -134,13 +133,14 @@ struct GraphicsEngine<B: Backend> {
     pub size: Extent2D,
     pub pixel_format: format::Format,
 
-    // Swapchain with backbuffer. Backbuffer is the secondary buffer which is not currently
+    // Swap-chain with a back-buffer. The back-buffer is the secondary buffer which is not currently
     // being displayed. The back buffer then becomes the front buffer (and vice versa).
     pub swapchain: Option<B::Swapchain>,
 
     // Render pass describing output of graphics pipeline, e.g. attachments and their format.
     pub render_pass: Option<B::RenderPass>,
-    // Framebuffers and corresponding stuff for each image in the swapchain.
+
+    // Frame buffers and corresponding stuff for each image in the swap-chain.
     pub frame_buffer_list: Vec<B::Framebuffer>,
     pub frame_buffer_fences: Vec<B::Fence>,
     pub frame_buffer_images: Vec<(B::Image, B::ImageView)>,
@@ -148,10 +148,13 @@ struct GraphicsEngine<B: Backend> {
     pub present_semaphores: Vec<B::Semaphore>,
     pub command_buffer_pools: Vec<hal::CommandPool<B, hal::Graphics>>,
     pub command_buffer_pools_lists: Vec<Vec<hal::command::CommandBuffer<B, hal::Graphics>>>,
-    // Index of the currently displayed image in the swapchain.
-    // Needed to aqquire buffers, semaphores and fences corresponding to the current image
-    // in the back buffer of the swapchain.
+
+    // Index of the currently displayed image in the swap-chain.
+    // Needed to acquire buffers, semaphores and fences corresponding to the current image
+    // in the back buffer of the swap-chain.
     pub sem_index: usize,
+
+    // Float32 sRGBA color which will be used to clear the surface.
     pub clear_color: [f32; 4],
 }
 
@@ -165,8 +168,9 @@ impl<B: Backend> GraphicsEngine<B> {
         pixel_format: format::Format,
         surface: &mut B::Surface,
     ) -> Self {
-        // 1. Create device swapchain config with window image size, two images (double buffering)
-        //    and where the value will represent the color on screen (and not depth, fo example).
+        // 1. Create device swap-chain config with the window image size, two images (e.g. double
+        //    buffering). The value will represent the just the color value on screen (and not
+        //    depth or stencil value, for example).
         let size = Extent2D {
             width: size.width as u32,
             height: size.height as u32,
@@ -182,8 +186,8 @@ impl<B: Backend> GraphicsEngine<B> {
             image_usage: image::Usage::COLOR_ATTACHMENT,
         };
 
-        // 2. Create a swapchain with backbuffer for surface and gpu. Backbuffer will contain the
-        //    images which are currently not in frontbuffer e.g. the image presented to the
+        // 2. Create a swap-chain with back-buffer for surface and gpu. Back-buffer will contain the
+        //    images which are currently not in front-buffer e.g. the image presented to the
         //    screen). Pretty much all the methods on gpu require unsafe scope.
         //    You have to destroy resources allocated on gpu by yourself.
         let (swapchain, backbuffer) =
@@ -192,14 +196,15 @@ impl<B: Backend> GraphicsEngine<B> {
         // Specify the layout the image will have before the render pass begins. Here we don't care
         // about the previous image since we're going to clear it anyway.
         let initial_layout = image::Layout::Undefined;
-        // Speficy the layout to transition when the render pass finishes. The image after this
-        // pass will be presented to the swapchain.
+        // Specify the layout to transition when the render pass finishes. The image after this
+        // pass will be presented to the swap-chain.
         let final_layout = image::Layout::Present;
+
         // Create an attachment describing the output of our render pass.
-        // Here we will use the same pixel format as in the swapchain.
-        // The number of samples per pixel is 1 since we are not doing any multisampling yet.
-        // We going to clear values in the attachment on every pass and store a new value.
-        // We are not using a stencil buffer, so we aren't adding any operations for it.
+        // Here we will use/output the same pixel format as in the swapchain.
+        // The number of samples per pixel is 1 since we are not doing any multi-sampling yet. We
+        // are going to just clear the values in the attachment on every pass and store a new
+        // value.
         let attachment = pass::Attachment {
             format: Some(pixel_format),
             samples: 1,
@@ -207,19 +212,22 @@ impl<B: Backend> GraphicsEngine<B> {
                 pass::AttachmentLoadOp::Clear,
                 pass::AttachmentStoreOp::Store,
             ),
+            // We are not using a stencil buffer, so we aren't adding any operations for it.
             stencil_ops: pass::AttachmentOps::new(
                 pass::AttachmentLoadOp::DontCare,
                 pass::AttachmentStoreOp::DontCare,
             ),
             layouts: initial_layout..final_layout,
         };
-        // Subpasses are ubsequent rendering operations. For example, post-processing effects.
-        // Here we reference color attachment 0.
+
+        // Here we reference our color attachment with index 0 defined above.
         let attachment_ref: pass::AttachmentRef = (0, image::Layout::ColorAttachmentOptimal);
+
+        // Sub-passes are the subsequent rendering operations. For example, post-processing effects.
         let subpass = pass::SubpassDesc {
             colors: &[attachment_ref],
             inputs: &[],         // Attachments to read from shader.
-            resolves: &[],       // Attachments for multisampling.
+            resolves: &[],       // Attachments for multi-sampling.
             depth_stencil: None, // Attachments for depth and stencil data.
             preserves: &[],      // Attachment which are not used, but should be preserved.
         };
@@ -232,20 +240,21 @@ impl<B: Backend> GraphicsEngine<B> {
             accesses: image::Access::empty()..rw_flags,
         };
 
-        // 3. Create a render pass which will specify how many color and depth buffers will be
-        // in the framebuffer be, also how many samples to use for each of them and how their
-        // contents should be handled throughout the rendering operations.
+        // 3. Create a render pass which will specify how many color buffers and depth buffers
+        //    will be in the frame buffer. Also how many samples to use for each of them and how
+        //    their contents should be handled throughout the rendering operations.
         let render_pass = unsafe {
             gpu.create_render_pass(&[attachment], &[subpass], &[dependency])
                 .unwrap()
         };
 
-        // 4. Create frame buffer.
+        // 4. Create frame buffers.
         let frame_buffer_extent = image::Extent {
             width: size.width,
             height: size.height,
             depth: 1,
         };
+
         // Create 2d color image views for every image in the swapchain.
         let frame_buffer_image_format = pixel_format;
         let frame_buffer_image_kind = image::ViewKind::D2;
@@ -253,8 +262,8 @@ impl<B: Backend> GraphicsEngine<B> {
         let frame_buffer_image_aspects = format::Aspects::COLOR;
         let frame_buffer_image_resource_range = image::SubresourceRange {
             aspects: frame_buffer_image_aspects,
-            levels: 0..1,
-            layers: 0..1,
+            levels: 0..1, // Mip-mapping levels with progressively lower resolution.
+            layers: 0..1, // Single layer image (could be used for stereographic 3D or in VR).
         };
         let frame_buffer_images = backbuffer
             .into_iter()
@@ -273,8 +282,8 @@ impl<B: Backend> GraphicsEngine<B> {
             })
             .collect::<Vec<(B::Image, B::ImageView)>>();
 
-        // For each frame buffer image, create a corresponding frame buffer with size
-        // equal to the window surface size.
+        // For each image in swap chain, create a corresponding frame buffer with the size
+        // equal to the window/surface size.
         let frame_buffer_list = frame_buffer_images
             .iter()
             .map(|&(_, ref buffer_image_view)| {
@@ -294,10 +303,18 @@ impl<B: Backend> GraphicsEngine<B> {
 
         // Allocate per frame buffer stuff.
         let frames_count = frame_buffer_images.len();
+
+        // Fences are mainly used to synchronize CPU work with rendering operation on GPU, whereas
+        // semaphores are used to synchronize operations within or across command queues on GPU.
         let mut frame_buffer_fences = Vec::with_capacity(frames_count);
         let mut command_buffer_pools = Vec::with_capacity(frames_count);
         let mut command_buffer_pools_lists = Vec::with_capacity(frames_count);
+
+        // Semaphores to signal that an image has been acquired and is ready for rendering.
         let mut acquire_semaphores = Vec::with_capacity(frames_count);
+
+        // Semaphores to signal that rendering has finished and presentation to swapchain
+        // can happen.
         let mut present_semaphores = Vec::with_capacity(frames_count);
 
         // Command pool flags.
@@ -320,22 +337,22 @@ impl<B: Backend> GraphicsEngine<B> {
                 command_buffer_pools_lists.push(command_buffer);
 
                 // Create semaphores.
-                let acquire_semaphor = gpu.create_semaphore();
-                let present_semaphor = gpu.create_semaphore();
-                acquire_semaphores.push(acquire_semaphor.unwrap());
-                present_semaphores.push(present_semaphor.unwrap());
+                let acquire_semaphore = gpu.create_semaphore();
+                let present_semaphore = gpu.create_semaphore();
+                acquire_semaphores.push(acquire_semaphore.unwrap());
+                present_semaphores.push(present_semaphore.unwrap());
             }
         }
 
         let sem_index = 0;
         let clear_color = [0.0, 0.0, 0.0, 1.0];
 
-        // Omg that's all!
         Self {
             gpu,
             queues,
             size,
             pixel_format,
+
             // All these to be destroyed manually in drop method:
             swapchain: Some(swapchain),
             render_pass: Some(render_pass),
@@ -351,14 +368,14 @@ impl<B: Backend> GraphicsEngine<B> {
         }
     }
 
-    /// Update will updatet state of graphics engine without drawing anything.
+    /// Update will update the state of graphics engine without drawing anything.
     /// For example, you can update the size of the swap chain images, etc.
     pub fn update(&mut self, color: [f32; 4]) {
         self.clear_color = color;
     }
 
     /// This function should be called on every frame to get the index of the current
-    /// frame in the swapchain and also to advance it to the next frame.
+    /// frame in the swapchain and also to advance the index to the next frame.
     fn advance_semaphore_index(&mut self) -> usize {
         if self.sem_index >= self.acquire_semaphores.len() {
             self.sem_index = 0;
@@ -368,9 +385,9 @@ impl<B: Backend> GraphicsEngine<B> {
         ret
     }
 
-    /// Draw will draw provided color.
+    /// Draw will draw engine state.
     pub fn draw(&mut self) -> Result<(), String> {
-        // Prep.
+        // Get current semaphore index and corresponding semaphores.
         let sem_index = self.advance_semaphore_index();
         let sem_acquire = &self.acquire_semaphores[sem_index];
         let sem_present = &self.present_semaphores[sem_index];
@@ -410,16 +427,16 @@ impl<B: Backend> GraphicsEngine<B> {
             h: self.size.height as i16,
         };
 
-        // Create clear color comand using float RGB format.
+        // Create "clear color" command using float RGB format.
         let clear_color_cmd = command::ClearColor::Sfloat(self.clear_color);
 
-        // Get command buffer.
+        // Get command buffer from the current frame pool.
         let mut command_buffer = match command_buffers.pop() {
             Some(buffer) => buffer,
             None => command_pool.acquire_command_buffer(),
         };
 
-        // Write commands.
+        // Write clear command.
         unsafe {
             // Start buffer.
             command_buffer.begin();
@@ -432,7 +449,7 @@ impl<B: Backend> GraphicsEngine<B> {
             command_buffer.finish();
         }
 
-        // Create queue submussion for the swapchain.
+        // Create queue submission for the swapchain.
         let submission = queue::Submission {
             command_buffers: iter::once(&command_buffer),
             wait_semaphores: iter::once((
@@ -442,7 +459,8 @@ impl<B: Backend> GraphicsEngine<B> {
             signal_semaphores: iter::once(&*sem_present),
         };
 
-        // Get gpu queue for graphics commands (we have at least one queue);
+        // Get gpu queue for graphics commands (we should have at least one
+        // queue).
         let command_queue = &mut self.queues.queues[0];
         unsafe {
             // Put submission to the command queue.
